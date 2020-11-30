b890b7a1b55d661fa926c6b236ba3abe9d46cc7c
Abdulrahman Elgendy <Abdul.Elgendy@outlook.com>
2020-11-30 16:07:17 -0600

    improve naming convention

--- FPL_team_selector/FPL_data_processing/__init__.py
+++
@@ -1,4 +0,0 @@
-from .player_metrics import *
-from .player_rating_generator import *
-from .team_selection import *
-from .utilities import *
--- FPL_team_selector/FPL_data_visualization/__init__.py
+++
@@ -1,1 +0,0 @@
-from .team_selection_visualization import *
